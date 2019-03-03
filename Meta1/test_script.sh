fbdc069f19781283630d78a2ec752ccafa2faf97
filipao223 <jpmontenegro67@gmail.com>
2019-03-03 00:12:03 +0000

    80 pontos

--- Meta1/test_script.sh
+++ Meta1/test_script.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+same=0
+different=0
+
 lex ~/Documents/COMP/Projeto/comp-projeto/Meta1/projeto.l && clang-3.9 ~/Documents/COMP/Projeto/comp-projeto/Meta1/lex.yy.c -o analisador1
 rm lex.yy.c
-
 
 for filename in ./*.dgo; do
     echo "Testing "$filename"...."
@@ -10,10 +12,15 @@ for filename in ./*.dgo; do
     #echo "${filename%.*}.out"
     if cmp -s "${filename%.*}.out" "output.txt"; then
         echo "Files are the same"
+        ((same++))
     else
         echo "Files are different"
+        ((different++))
     fi
 done
 
 rm output.txt
 rm analisador1
+
+echo "$same files are equal"
+echo "$different files are different"
