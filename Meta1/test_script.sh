eb3d32bb8606a576985e578ac54595fb364eb964
filipao223 <jpmontenegro67@gmail.com>
2019-03-03 12:36:17 +0000

    Updated script, if file is errors.dgo, run without -l. If -l isomitted dont print SEMICOLON

--- Meta1/test_script.sh
+++ Meta1/test_script.sh
@@ -8,8 +8,14 @@ rm lex.yy.c
 
 for filename in ./*.dgo; do
     echo "Testing "$filename"...."
-    ./analisador1 -l < $filename > output.txt
-    #echo "${filename%.*}.out"
+
+    if [ "$filename" = "./errors.dgo" ]; then
+        ./analisador1 < $filename > output.txt
+    else
+        ./analisador1 -l < $filename > output.txt
+    fi
+    
+
     if cmp -s "${filename%.*}.out" "output.txt"; then
         echo "Files are the same"
         ((same++))
@@ -21,6 +27,13 @@ done
 
 rm output.txt
 rm analisador1
+
+#Move the .l file to the desktop, rename it and zip it
+project_dir=$(xdg-user-dir DOCUMENTS)/COMP/Projeto/comp-projeto/Meta1
+cp $project_dir/projeto.l $(xdg-user-dir DESKTOP)/gocompiler.l
+cd $(xdg-user-dir DESKTOP)
+zip gocompiler.zip gocompiler.l
+cd -
 
 echo "$same files are equal"
 echo "$different files are different"
