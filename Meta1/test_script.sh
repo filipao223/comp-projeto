ceda5471988a6f70074da1ef2bc84840890294fc
filipao223 <jpmontenegro67@gmail.com>
2019-03-19 10:47:30 +0000

    Esqueci-me de incluir o novo ficheiro lex, alteraçoes no script de teste

--- Meta1/test_script.sh
+++ Meta1/test_script.sh
@@ -7,9 +7,9 @@ project_dir=$(xdg-user-dir DOCUMENTS)/COMP/Projeto/comp-projeto/Meta1
 
 cd $project_dir
 
-lex projeto.l 
-clang-3.9 lex.yy.c -o analisador1
-cp analisador1 $project_dir/CasosTeste/GitLab
+lex gocompiler.l 
+clang-3.9 lex.yy.c -o gocompiler
+cp gocompiler $project_dir/CasosTeste/GitLab
 
 cd $project_dir/CasosTeste/GitLab
 
@@ -17,9 +17,9 @@ for filename in ./*.dgo; do
     echo "Testing "$filename"...."
 
     if [ "$filename" = "./errors.dgo" ]; then
-        ./analisador1 < $filename > output.txt
+        ./gocompiler < $filename > output.txt
     else
-        ./analisador1 -l < $filename > output.txt
+        ./gocompiler -l < $filename > output.txt
     fi
     
 
@@ -28,15 +28,16 @@ for filename in ./*.dgo; do
         ((same++))
     else
         echo "Files are different"
+        mv output.txt ${filename%.*}_ours.out
         ((different++))
     fi
 done
 
 rm output.txt
-rm analisador1
+rm gocompiler
 
 #Move the .l file to the desktop, rename it and zip it
-cp $project_dir/projeto.l $(xdg-user-dir DESKTOP)/gocompiler.l
+cp $project_dir/gocompiler.l $(xdg-user-dir DESKTOP)/gocompiler.l
 cd $(xdg-user-dir DESKTOP)
 zip gocompiler.zip gocompiler.l
 cd -
