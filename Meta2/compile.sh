d3b2690f56f8a578bd71cc1227a4315485e0e367
filipao223 <jpmontenegro67@gmail.com>
2019-03-23 12:11:21 +0000

    Codigo base, ficheiro compile.sh usado para compilar com ou nao informaçoes de debug. Test_script n faz nada ainda

---
+++ Meta2/compile.sh
@@ -0,0 +1,7 @@
+if [ $# -eq 1 ]; then
+    if [ "$1" = "debug" ]; then
+        lex gocompiler.l && yacc -d --debug --verbose gocompiler.y && cc -o gocompiler lex.yy.c y.tab.c -lm
+    fi
+else
+    lex gocompiler.l && yacc -d gocompiler.y && cc -o gocompiler lex.yy.c y.tab.c -lm
+fi
