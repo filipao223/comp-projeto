a7a433d3ca8f2a32ac5c1026684b1018773e0a06
flavioafernandes <flavio98.fernandes@gmail.com>
2019-05-23 20:26:42 +0100

    Calls, operações matematicas, returns a funcionar

--- Meta4/gen_code.c
+++ Meta4/gen_code.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
+#include <ctype.h>
 #include "structs.h"
 #include "ast.h"
 #include "gen_code.h"
 
+#define MAX_WORD 20
+
 int count = 0;
 
-int starts_with(const char *pre, const char *str)
-{
+int starts_with(const char *pre, const char *str){
     if( strncmp(pre, str, strlen(pre)) == 0) return 1;
 
     return 0;
+}
+
+char* to_lower_case(char* word){
+    for(int i = 0; word[i]; i++){
+        word[i] = tolower(word[i]);
+    }
+
+    return word;
 }
 
 char* type_of(ast_node* node){
@@ -37,6 +45,14 @@ char* type_of(ast_node* node){
 int is_operation(char* op){
     if(strcmp(op,"Eq") == 0 || strcmp(op,"Ne") == 0 || strcmp(op,"Lt") == 0
     || strcmp(op,"Gt") == 0 || strcmp(op,"Le") == 0 || strcmp(op,"Ge") == 0 ){
+        return 1;
+    }
+
+    return 0;
+}
+
+int is_math_operation(char* op){
+    if(strcmp(op,"Mul") == 0 || strcmp(op,"Div") == 0 || strcmp(op,"Sub") == 0){
         return 1;
     }
 
@@ -90,6 +106,8 @@ void generate_code(ast_node *root){
 void generate_func(ast_node* node){
     //printf("GENERATE_FUNC: %s\n", node->name);
     //primeiro for para os FuncDecl
+    //tipo de return
+    char* type = "";
     for(int i = 0; i < node -> num_children; i++){
         //for para os FuncHeader ou FuncBody
         //for(int j = 0; j < node->children[i]->num_children; j++){
@@ -99,7 +117,7 @@ void generate_func(ast_node* node){
                 ast_node* temp = node->children[i];
                 char *id = temp->children[0]->id;
                 //printf("ID: %s\n", id);
-                char *type = type_of(temp->children[1]);
+                type = type_of(temp->children[1]);
                 if(strcmp(type,"") == 0){
                     printf("define @%s(", id);
                 }else{
@@ -112,7 +130,7 @@ void generate_func(ast_node* node){
 
             if(strcmp(node->children[i]->name, "FuncBody") == 0){
                 ast_node* temp = node->children[i];
-                generate_func_body(temp);
+                generate_func_body(temp,type);
                 printf("}\n");
             }
 
@@ -163,31 +181,86 @@ char *generate_var_decl(ast_node* node, int global){
     return "";    
 }
 
-char *generate_parse_args(ast_node* node){
+char *generate_math_operations(ast_node* node){
 
-    //char* call = "";
+    char* op = "";
+    char* val0 = "";
+    char* val1 = "";
+    char* type = "";
+    char* line_name = (char *)malloc((MAX_WORD)*sizeof(char));
+
+    if(is_math_operation(node->children[0]->name)){
+        val0 = generate_math_operations(node->children[0]);
+    }
+
+    if(is_math_operation(node->children[1]->name)){
+        val1 = generate_math_operations(node->children[1]);
+    }
+
+    op = to_lower_case(node->name);
+    type = type_of(node);
+    if(strcmp(val0,"") == 0) val0 = node->children[0]->id;
+    if(strcmp(val1,"") == 0) val1 = node->children[1]->id;
+    sprintf(line_name, "%%%s%d", op, count);
+    count = count + 1;
+
+
+    printf("%s = %s %s %s, %s\n", line_name, op, type,val0, val1);
+
+    return line_name;
+
+}
+
+char* generate_call(ast_node* node, char* value, char* value_type){
+
+    char call[MAX_WORD];    
+    ast_node* temp = node->children[1];
+    char* func = temp->children[0]->id;
+    char* arguments = temp->children[1]->id;
+    sprintf(call, "%s%d", "%call", count);
+    count=count+1;
+    strcpy(value,call);  
+    printf("%s = call %s @%s(%s)\n", call, value_type,func,arguments);
+    return value;
+}
+
+char *generate_assign(ast_node* node){
+
+    
     char* arg = node->children[0]->id;
     char* value = node->children[1]->id;
     char* arg_type = type_of(node->children[0]);
     char* value_type = type_of(node->children[1]);
 
-    /*if(strcmp(node->children[1]->name, "Call") == 0){
-        printf("IN");
-        ast_node* temp = node->children[1];
-        char* func = temp->children[0]->id;
-        char* arguments = temp->children[1]->id;
-        sprintf(call, "%s%d", "call", count);
-        count++;
-        value = call;
-        printf("%%%s = call %s @%s(%s)\n", call, value_type,func,arguments);       
-    }*/
+    if(strcmp(node->children[1]->name, "Call") == 0){
+        value = generate_call(node,value,value_type);
+    }
+
+    if(is_math_operation(node->children[1]->name)){
+        value = generate_math_operations(node->children[1]);
+    }
 
     printf("store %s %s, %s* %%%s\n", value_type, value, arg_type, arg);
 
     return "";
 }
 
-char *generate_func_body(ast_node* node){
+char *generate_return(ast_node* node, char* return_type){
+
+    char* op = "";
+
+    for (int i = 0; i < node->num_children; i++){
+        if(is_math_operation(node->children[i]->name)){
+            op = generate_math_operations(node->children[i]);
+        }
+    }
+
+    printf("ret %s %s\n", return_type, op);
+    
+    return "";
+}
+
+char *generate_func_body(ast_node* node, char* return_type){
 
     for (int i = 0; i < node->num_children; i++){
         if(strcmp(node->children[i]->name, "If") == 0){
@@ -217,7 +290,11 @@ char *generate_func_body(ast_node* node){
         }
 
         if(strcmp(node->children[i]->name, "Assign") == 0){
-            generate_parse_args(node->children[i]);    
+            generate_assign(node->children[i]);    
+        }
+
+        if(strcmp(node->children[i]->name, "Return") == 0){
+            generate_return(node->children[i], return_type);    
         }
     }
    
--- Meta4/gen_code.h
+++ Meta4/gen_code.h
@@ -5,15 +5,19 @@
 #include "structs.h"
 
 int starts_with(const char *, const char *);
+char* to_lower_case(char* );
 char* extract_between(const char *, const char *, const char *);
 char* type_of(ast_node*);
 char* type_of_operation(ast_node* );
 void generate_code(ast_node *);
 void generate_func(ast_node* );
 char *generate_func_params(ast_node* );
-char *generate_func_body(ast_node*);
-char *generate_var_decl(ast_node* , int);
+char *generate_func_body(ast_node*, char*);
+char *generate_var_decl(ast_node* ,int);
 int is_operation(char* );
-char *generate_parse_args(ast_node* );
+char *generate_assign(ast_node* );
+char *generate_math_operations(ast_node* );
+char *generate_return(ast_node* , char* );
+char* generate_call(ast_node* , char* , char* );
 
 #endif
